f8fc7cb38f95df3918075cb1b5d4910c143f3049
Avexen <98061293+Avexen@users.noreply.github.com>
2022-04-19 22:15:44 +0200

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int wybor, num1, num2;
+
+    while(true)
+    {
+
+        cout    << "╔═════════════════════════════════╗\n"
+                << "║                                 ║\n"
+                << "║     CALCULATOR VERSION 1.10     ║\n"
+                << "║                                 ║\n"
+                << "╚═════════════════════════════════╝\n";
+
+        cout << "Prosze wybrac opcje: \n1. Dodawanie, \n2. Odejmowanie, \n3. Mnozenie, \n4. Dzielenie";
+        cout << "\nWybieram opcje: ";
+        cin >> wybor;
+
+        switch(wybor){
+            case 1:
+                cout << "\nWybrano opcje dodawnia.";
+                cout << "\nProsze podac pierwsza liczbe: ";
+                cin >> num1;
+                cout << "Prosze podac druga liczbe: ";
+                cin >> num2;
+                cout << "Twoj wynik to: " << num1 + num2 << "\n";
+                break;
+        }
+    }
+}
